97116722d1e9bebfa735604ae4b98564e30882a3
Jarod Wilson <jaroddwilson@gmail.com>
2022-11-05 13:59:42 -0400

    Completed assignment

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,30 @@
 class Triangle
-  # write code here
+
+  def initialize(side_a, side_b, side_c)
+    @triangle = [side_a, side_b, side_c].sort
+  end
+
+  def kind
+    if @triangle[0] <= 0
+      raise TriangleError
+    end
+    if @triangle[0] + @triangle[1] <= @triangle[2]
+      raise TriangleError
+    end
+    if @triangle[0] == @triangle[1]
+      if @triangle[0] == @triangle[2]
+        :equilateral
+      else
+        :isosceles
+      end
+    elsif @triangle[1] == @triangle[2]
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  class TriangleError < StandardError
+  end
+
 end
